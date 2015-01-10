510319682700577739b76509015611d2b3a2ef18
jake.vanorder@gmail.com <jake.vanorder@gmail.com>
2015-01-10 13:58:15 -0500

    Added project.

---
+++ VOKAlertControllerExample/VOKAlertControllerExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VOKAlertControllerExample
+//
+//  Created by mrJacob on 1/10/15.
+//  Copyright (c) 2015 Vokal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
